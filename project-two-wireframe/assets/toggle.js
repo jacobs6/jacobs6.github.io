11f3920fce793572d53deaacaa9409438661da6b
jacobs6 <jacobs6@goshen.edu>
2020-10-21 12:14:11 -0400

    episode guide and photos
    added frames for photos and dropdown functions for episode guide

--- project-two-wireframe/assets/toggle.js
+++ project-two-wireframe/assets/toggle.js
@@ -5,3 +5,99 @@ $(document).ready(function (){
         $("#first-episodes").toggleClass("visible");
         });
 });
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#second-button').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#second-episodes").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#third-button').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#third-episodes").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#fourth-button').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#fourth-episodes").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#fifth-button').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#fifth-episodes").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#sixth-button').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#sixth-episodes").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#seventh-button').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#seventh-episodes").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#eighth-button').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#eighth-episodes").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#ninth-button').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#ninth-episodes").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#tenth-button').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#tenth-episodes").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#eleventh-button').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#eleventh-episodes").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#twelfth-button').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#twelfth-episodes").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#thirteenth-button').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#thirteenth-episodes").toggleClass("visible");
+        });
+});
